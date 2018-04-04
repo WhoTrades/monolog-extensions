2898fe60abfbf7816b6a16ba74cfd5e23184f77f
Anton Gorlanov <antonxacc@gmail.com>
2018-04-04 17:43:35 +0300

    [*] Add new consts to LoggerWt #WTA-1572

--- LoggerWt.php
+++ LoggerWt.php
@@ -12,4 +12,7 @@ class LoggerWt extends Logger
     const CONTEXT_EXCEPTION = 'exception'; // ag: Object of type \Throwable
     const CONTEXT_DUMP = 'dump'; // ag: Force send log to sentry
     const CONTEXT_COLLECT_TRACE = 'collect_trace'; // ag: Add to log collected trace
+    const CONTEXT_PROCESS = 'process';
+    const CONTEXT_STATUS = 'status';
+    const CONTEXT_REASON = 'reason';
 }
