0a8272bfc6ceb35bc8e99538f39a93a233ab9bcc
Anton Gorlanov <antonxacc@gmail.com>
2018-06-15 16:01:58 +0300

    [*] Add OperationProcessor #WTA-1685

--- Processor/OperationProcessor.php
+++ Processor/OperationProcessor.php
@@ -24,31 +24,27 @@ class OperationProcessor
      */
     public function __invoke(array $record)
     {
-        if ((!isset($record['context'][LoggerWt::CONTEXT_OPERATIONS])
-                || !is_array($record['context'][LoggerWt::CONTEXT_OPERATIONS])
-            ) && empty($record['context'][LoggerWt::CONTEXT_FINISH_LOGGING])) {
-            return $record;
-        }
+        if (isset($record['context'][LoggerWt::CONTEXT_OPERATIONS]) && is_array($record['context'][LoggerWt::CONTEXT_OPERATIONS])) {
+            foreach ($record['context'][LoggerWt::CONTEXT_OPERATIONS] as $operationName => $operationAction) {
+                switch ($operationAction) {
+                    case self::ACTION_START:
+                        if (!isset($this->operations[$operationName])) {
+                            $this->operations[$operationName] = new Item\Operation($operationName);
+                        }
 
-        foreach ($record['context'][LoggerWt::CONTEXT_OPERATIONS] as $operationName => $operationAction) {
-            switch ($operationAction) {
-                case self::ACTION_START:
-                    if (!isset($this->operations[$operationName])) {
-                        $this->operations[$operationName] = new Item\Operation($operationName);
-                    }
+                        $this->operations[$operationName]->start();
+                        $record['extra'][LoggerWt::CONTEXT_OPERATIONS][$operationName] = $this->operations[$operationName]->getInfoArray();
+                        break;
+                    case self::ACTION_STOP:
+                        if (!isset($this->operations[$operationName])) {
+                            $this->operations[$operationName] = new Item\Operation($operationName);
+                        }
 
-                    $this->operations[$operationName]->start();
-                    $record['extra'][LoggerWt::CONTEXT_OPERATIONS][$operationName] = $this->operations[$operationName]->getInfoArray();
-                    break;
-                case self::ACTION_STOP:
-                    if (!isset($this->operations[$operationName])) {
-                        $this->operations[$operationName] = new Item\Operation($operationName);
-                    }
-
-                    $this->operations[$operationName]->stop();
-                    $record['extra'][LoggerWt::CONTEXT_OPERATIONS][$operationName] = $this->operations[$operationName]->getInfoArray();
-                    break;
-                default:
+                        $this->operations[$operationName]->stop();
+                        $record['extra'][LoggerWt::CONTEXT_OPERATIONS][$operationName] = $this->operations[$operationName]->getInfoArray();
+                        break;
+                    default:
+                }
             }
         }
 
